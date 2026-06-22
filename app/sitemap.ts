bd2489fc833282853a005d15ce2c3c3e6747a126
geniusvisac <geniusvisac@gmail.com>
2026-06-21 20:13:49 -0600

    Add files via upload

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -36,6 +36,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${BASE_URL}/viajes`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: `${BASE_URL}/privacidad`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
